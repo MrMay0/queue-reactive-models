e4f827f8821ca5fb7b5cf64d856a681e4e1f496e
MrMay0 <ulysse.ristorcelli@telecom-paris.fr>
2026-03-05 11:23:41 +0000

    Download month of November of Bund Future

---
+++ run_cost.py
@@ -0,0 +1,4 @@
+from src.data.download import estimate_cost
+
+if __name__ == "__main__":
+    print(estimate_cost())
---
+++ run_download.py
@@ -0,0 +1,4 @@
+from src.data.download import download_data
+
+if __name__ == "__main__":
+    download_data()
